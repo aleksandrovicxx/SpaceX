5e81dd0d18ec8fe1c203217383645954735b6b1b
aleksandrovicxx <aleksandrovicmilos99@gmail.com>
2024-03-02 18:43:33 +0100

    Company file fixed

--- src/components/company/Company.jsx
+++ src/components/company/Company.jsx
@@ -7,23 +7,21 @@ const Company = ({companyInfo}) =>{
             <h1>{companyInfo.name}</h1>
             <p>Founder: <i>{companyInfo.founder}</i></p>
             <p>Employees: <i>{companyInfo.employees}</i></p>
-            <p>
-                <div>
-                   {Object.keys(companyInfo.headquarters).map(key => (
-                   <div key={key}>
-                    {" "}
-                    {key}:{companyInfo.headquarters[key]}{" "} </div>)
-                   )}
-                </div>
-                <div>
-                    <h4>Link:</h4>
-                    {Object.keys(companyInfo.links).map(key => (
-                        <div key={key}>
-                            <a href={companyInfo.links[key]}>{companyInfo.links[key]} </a>
-                        </div>
-                    ))}
-                </div>
-            </p>
+            <div>
+                {Object.keys(companyInfo.headquarters).map(key => (
+                <div key={key}>
+                {" "}
+                {key}:{companyInfo.headquarters[key]}{" "} </div>)
+                )}
+            </div>
+            <div>
+                <h4>Link:</h4>
+                {Object.keys(companyInfo.links).map(key => (
+                    <div key={key}>
+                        <a href={companyInfo.links[key]}>{companyInfo.links[key]} </a>
+                    </div>
+                ))}
+            </div>
         </div>
     </div>
 }
